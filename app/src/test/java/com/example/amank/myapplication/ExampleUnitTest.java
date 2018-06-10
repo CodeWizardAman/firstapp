b48c89fddf02e5349de50a20dd08887f99609854
Aman Kesarwani <amank2410@gmail.com>
2018-06-10 14:57:14 +0530

    First commit

---
+++ app/src/test/java/com/example/amank/myapplication/ExampleUnitTest.java
@@ -0,0 +1,26 @@
+package com.example.amank.myapplication;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void sum_all() {
+
+        int i = 20 + 30;
+
+        assertEquals(50, i);
+
+    }
+}
